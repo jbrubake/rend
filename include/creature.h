5376c99270abb7e1e1b6ffa7ede573a2bf0f63f7
cuddlycultist <cc@cuddlycultist.com>
2012-10-13 15:53:30 -0400

    Documented reflist, an oversight.

--- include/creature.h
+++ include/creature.h
@@ -26,91 +26,10 @@ typedef struct part_t {
 
 typedef struct body_t {
 	part_t* rootpart;
+	fraction_t guard;
+	fraction_t concentration;
+	fraction_t stamina;
 } body_t;
-
-enum {
-	// Body sections
-	PART_HEAD,
-	PART_NECK,
-	PART_CHEST,
-	PART_ABDOMEN,
-	PART_PELVIS,
-	PART_LEFTLEG,
-	PART_RIGHTLEG,
-	PART_LEFTARM,
-	PART_RIGHTARM,
-	PART_LEFTHAND,
-	PART_RIGHTHAND,
-
-	// Bones
-	PART_SKULL,
-	PART_UPPERSPINE,
-	PART_LOWERSPINE,
-	PART_RIBS,
-	PART_PELVICBONE,
-	PART_LEFTFEMUR,
-	PART_RIGHTFEMUR,
-
-	// Organs
-	PART_BRAIN,
-	PART_HEART,
-	PART_LEFTLUNG,
-	PART_RIGHTLUNG,
-	PART_SPLEEN,
-	PART_GUT
-};
-#define PART(nm, hlth, nec, imp, pn, bld) {.name = nm, .vitality={hlth, hlth}, .necessary=nec, .importance=imp, .pain=pn, .bleeds=bld}
-static part_t parts[] = {
-	// PART(Name, health, necessary, importance level, pain, bleeding)
-	PART("Head",         16, 0, 0,  8, 4),
-	PART("Neck",         12, 0, 0,  8, 16),
-	PART("Chest",        64, 0, 0,  8, 4),
-	PART("Abdomen",      64, 0, 0,  8, 4),
-	PART("Pelvis",       48, 0, 0,  8, 4),
-	PART("Left Leg",     32, 0, 0,  8, 6),
-	PART("Right Leg",    32, 0, 0,  8, 6),
-	PART("Left Arm",     24, 0, 0,  8, 6),
-	PART("Right Arm",    24, 0, 0,  8, 6),
-	PART("Left Hand",     8, 0, 0,  8, 2),
-	PART("Right Hand",    8, 0, 0,  8, 2),
-
-	PART("Skull",        12, 0, 1, 16,  0),
-	PART("Upper Spine",   8, 0, 1, 12,  0),
-	PART("Lower Spine",   8, 0, 1, 12,  0),
-	PART("Ribs",         12, 0, 1, 16,  0),
-	PART("Pelvic Bone",  20, 0, 1, 12,  0),
-	PART("Left Femur",   20, 0, 1, 16, 16),
-	PART("Right Femur",  20, 0, 1, 16, 16),
-
-	PART("Brain",         8, 1, 1,  0,  4),
-	PART("Heart",        12, 1, 1,  8, 32),
-	PART("Left Lung",    12, 1, 1,  8,  4),
-	PART("Right Lung",   12, 1, 1,  8,  4),
-	PART("Spleen",       12, 0, 1,  8, 24),
-	PART("Gut",          24, 0, 1, 12, 16),
-};
-static struct {int parent; int child;} connections[] = {
-	// Main body bits
-	{PART_HEAD, PART_NECK},
-	{PART_NECK, PART_CHEST},
-	{PART_CHEST, PART_ABDOMEN},
-	{PART_ABDOMEN, PART_PELVIS},
-	{PART_PELVIS, PART_LEFTLEG},
-	{PART_PELVIS, PART_RIGHTLEG},
-
-	// Right arm
-	{PART_CHEST, PART_RIGHTARM},
-	{PART_RIGHTARM, PART_RIGHTHAND},
-
-	// Left arm
-	{PART_CHEST, PART_LEFTARM},
-	{PART_LEFTARM, PART_LEFTHAND},
-};
-
-enum {
-	CREATURE_HUMAN,
-	CREATIRE_GOBLIN,
-};
 
 typedef struct creature_t
 {
@@ -119,32 +38,11 @@ typedef struct creature_t
 //	soul_t *soul;
 } creature_t;
 
-static void print_part(const part_t* p) {
-	printf("%s: (%d/%d) [", p->name, p->vitality.v, p->vitality.max);
-	int k=0;
-	if (p->necessary)   {printf(k?" %s":"%s",               "necessary"); k++;}
-	if (!p->importance) {printf(k?" %s":"%s",               "important"); k++;}
-	if (p->pain)        {printf(k?" pain=%d":"pain=%d",     p->pain);     k++;}
-	if (p->bleeds)      {printf(k?" bleeds=%d":"bleeds=%d", p->bleeds);   k++;}
-	printf("]\n");
-}
+enum {
+	CREATURE_HUMAN,
+	CREATIRE_GOBLIN,
+};
 
-static void print_parttree(const part_t* p, int depth) {
-	int i; for (i=0; i<depth; i++) {printf("\t");}
-	printf(depth?"->%s\n":"%s\n", p->name);
-	reflist_node_t* n = p->children.f;
-	while (n) {print_parttree(n->data, depth+1); n = n->n;}
-}
-
-void creature_test() {
-	int i;
-	for (i=0; i<sizeof(parts)/sizeof(*parts); i++) {
-		print_part(parts+i);
-	}
-	for (i=0; i<sizeof(connections)/sizeof(*connections); i++) {
-		reflist_add(&parts[connections[i].parent].children, parts + connections[i].child);
-	}
-	print_parttree(parts + PART_HEAD, 0);
-}
+void creature_test();
 
 #endif
